e207dc41c181515daa400d1664d4385e734bb422
GuidoFC <guidofigueroa96@gmail.com>
2024-11-28 10:22:13 +0100

    pruebas

--- vite/vite-project/vue-project/src/views/HomeView.vue
+++ vite/vite-project/vue-project/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+import {ref, onMounted} from "vue";
+
+const saluda = ref("Hola");
+
+function majuscula(){
+  saluda.value = saluda.value.toUpperCase();
+}
+
+
 </script>
 
 <template>
   <main>
+    <button @click="majuscula()">Majuscula</button>/
+
     <TheWelcome />
   </main>
 </template>
