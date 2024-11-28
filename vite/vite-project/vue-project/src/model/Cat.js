506bda7cd2ce186acb7af4c4bf894712ec4f7e19
GuidoFC <guidofigueroa96@gmail.com>
2024-11-28 10:27:07 +0100

    clase cat

---
+++ vite/vite-project/vue-project/src/model/Cat.js
@@ -0,0 +1,56 @@
+export class Cat {
+  #id;
+  #name;
+  #url;
+  #widht;
+  #height;
+
+
+  constructor(id, name, url, widht, height) {
+    this.#id = id;
+    this.#name = name;
+    this.#url = url;
+    this.#widht = widht;
+    this.#height = height;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  set id(value) {
+    this.#id = value;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
+  }
+
+  get url() {
+    return this.#url;
+  }
+
+  set url(value) {
+    this.#url = value;
+  }
+
+  get widht() {
+    return this.#widht;
+  }
+
+  set widht(value) {
+    this.#widht = value;
+  }
+
+  get height() {
+    return this.#height;
+  }
+
+  set height(value) {
+    this.#height = value;
+  }
+}
