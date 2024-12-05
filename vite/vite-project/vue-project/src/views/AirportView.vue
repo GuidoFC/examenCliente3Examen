437a7323dc3896eed32d99b95c3b458e4f7d9b49
GuidoFC <guidofigueroa96@gmail.com>
2024-12-05 11:43:30 +0100

    Ya funciona

--- vite/vite-project/vue-project/src/views/AirportView.vue
+++ vite/vite-project/vue-project/src/views/AirportView.vue
@@ -1,10 +1,14 @@
 <template>
   <div >
-  <h1>Aeropuertos</h1>
+  <h1>Aeropuertos <span v-if="valorCerca"> de {{valorCerca.toUpperCase()}}</span> !</h1>
 
+    <input v-model="valorCerca" placeholder="pmi" @input="cercaAeropuerto">
+    <ul>
+      <li v-for="aeroport in aeroportsFilter">
+        {{aeroport.codi}} - {{aeroport.nom}} - {{aeroport.ciutat}}
+      </li>
+    </ul>
 
-    <input type="text" v-model="BuscarAeropuerto">
-    <p>Los aeropuerto encontrados son: {{BuscarAeropuerto}}</p>
   </div>
 </template>
 
@@ -12,13 +16,29 @@
 <script setup>
 import {onMounted, ref} from "vue";
 import {AeropoertService} from "@/service/AeropoertService.js";
-const BuscarAeropuerto = ref("pmi");
+
+
+const aeroports = ref([])
+const aeroportsFilter = ref([])
+
+const valorCerca = ref("")
+
 const serviceAeropuerto = new AeropoertService();
+
+async function carregarAeropuertos(){
+  aeroports.value = await serviceAeropuerto.load();
+}
+
+async function cercaAeropuerto(){
+  aeroportsFilter.value =  serviceAeropuerto.cerca(aeroports.value, valorCerca.value)
+}
+
 
 onMounted(async () => {
 
-  const dataRecibido = await serviceAeropuerto.load();
-  console.log(dataRecibido);
+ await carregarAeropuertos();
+ // si queremos que la lista inifica sean todos los aeropuertos
+ aeroportsFilter.value = aeroports.value
 
 })
 
